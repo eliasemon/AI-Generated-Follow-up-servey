cffb0b217b09ae4b68d488366f5460ffc0872f74
Elias Emon <eliasemon0025@gmail.com>
2024-12-31 05:20:21 +0600

    refactor: fix some ui related issues.

--- src/components/AnswerHistory.tsx
+++ src/components/AnswerHistory.tsx
@@ -8,6 +8,8 @@ interface AnswerHistoryProps {
 
 export const AnswerHistory: React.FC<AnswerHistoryProps> = ({ questions }) => {
   return (
+    <div className='bg-white rounded-lg max-w-2xl w-full max-h-[80vh] overflow-hidden'>
+    <div className="p-4 overflow-y-auto max-h-[calc(80vh-4rem)]" >
     <div className="space-y-6">
       {questions.map((question, index) => (
         <div key={question.id} className="border-b pb-4 last:border-b-0">
@@ -19,6 +21,8 @@ export const AnswerHistory: React.FC<AnswerHistoryProps> = ({ questions }) => {
           </div>
         </div>
       ))}
+    </div>
+    </div>
     </div>
   );
 }
