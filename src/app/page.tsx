df64cdae0a77073ffe2152bb6cc17416f7daad18
Elias Emon <eliasemon0025@gmail.com>
2025-02-12 19:24:37 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
       <QuestionAnswer />
+      <h1> Hello World!</h1>
     </div>
   );
 }
